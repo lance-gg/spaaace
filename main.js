798ab5f24870a3e39de548d3fe98557596acce89
Opher Vishnia <opherv@gmail.com>
2016-06-16 22:44:07 +0300

    seperated Incheon from Spaaace

---
+++ main.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, './index.html');
+
+const server = express();
+
+server.get('/', function (req, res) {
+    res.sendFile(INDEX)
+});
+
+
+server.use('/', express.static(path.join(__dirname, '.')));
+
+var requestHandler = server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(requestHandler);
+
+
+/*
+    Game logic
+ */
+
+const SpaaaceServerEngine = require(path.join(__dirname, 'src/SpaaaceServerEngine.js'));
+const SpaaaceGameEngine = require(path.join(__dirname, 'src/SpaaaceGameEngine.js'));
+
+const gameEngine = new SpaaaceGameEngine();
+const serverEngine = new SpaaaceServerEngine(io, gameEngine);
+serverEngine.start();
+
+/*
+    Server IO
+ */
+
+io.on('connection', onClientConnect);
+
+function onClientConnect(socket){
+    serverEngine.onPlayerConnected(socket);
+}
+
+
+
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
