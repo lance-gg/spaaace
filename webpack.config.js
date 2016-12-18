3bc95534f4acc600a83b1ef05d9e48b6914f8f9e
Gary Weiss <garyweiss74@gmail.com>
2016-12-18 23:23:01 +0200

    support npm link of incheon

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
-var path = require("path");
+var path = require('path');
+var fs = require('fs');
 
 module.exports = {
-    entry: "./src/client/clientEntryPoint.js",
+    entry: './src/client/clientEntryPoint.js',
     output: {
-        path: path.join(__dirname,"dist"),
-        filename: "bundle.js"
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
     },
     module: {
         preLoaders: [
-            { test: /\.json$/, exclude: /node_modules/, loader: 'json'},
-            { test: /(ServerEngine)/, loader: "null" }
+            { test: /\.json$/, exclude: /node_modules/, loader: 'json' },
+            { test: /(ServerEngine)/, loader: 'null' }
         ],
         loaders: [
-            { test: /\.css$/, loader: "style!css" },
+            { test: /\.css$/, loader: 'style!css' },
             {
                 test: /\.js$/,
                 include: [
-                    path.resolve(__dirname, "src"),
-                    path.resolve(__dirname, "node_modules/incheon")
+                    path.resolve(__dirname, 'src'),
+                    path.resolve(__dirname, 'node_modules/incheon'),
+                    fs.realpathSync('./node_modules/incheon')
                 ],
                 loader: 'babel-loader',
                 query: {
@@ -25,5 +27,7 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    resolve: { fallback: path.join(__dirname, 'node_modules') },
+    resolveLoader: { fallback: path.join(__dirname, 'node_modules') }
 };
