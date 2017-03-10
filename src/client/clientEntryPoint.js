424fafc4dc12bbfd5f895aa990142f050effbb1f
Gary Weiss <fourvelocity@gmail.com>
2017-03-10 15:58:45 +0200

    use softer bending

--- src/client/clientEntryPoint.js
+++ src/client/clientEntryPoint.js
@@ -8,12 +8,12 @@ require('../../assets/sass/main.scss');
 // is sent to both game engine and client engine
 const defaults = {
     traceLevel: 1000,
-    delayInputCount: 3,
+    delayInputCount: 8,
     clientIDSpace: 1000000,
     syncOptions: {
         sync: qsOptions.sync || 'extrapolate',
-        localObjBending: 0.6,
-        remoteObjBending: 0.6
+        localObjBending: 0.2,
+        remoteObjBending: 0.5
     }
 };
 let options = Object.assign(defaults, qsOptions);
