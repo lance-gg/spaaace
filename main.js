bac20389616cd9d8982621b231ee338ded3facd5
Yang Zhang <yaaang@gmail.com>
2017-09-18 21:32:01 -0700

    Add server option to inject artificial latency
    Exaggerating latency helps devs understand lance's sync behavior.

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, './index.html');
+const FAKE_LAG = process.env.FAKE_LAG || 0;
 
 // define routes and socket
 const server = express();
@@ -13,6 +14,21 @@ server.get('/', function(req, res) { res.sendFile(INDEX); });
 server.use('/', express.static(path.join(__dirname, '.')));
 let requestHandler = server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 const io = socketIO(requestHandler);
+
+if (FAKE_LAG) {
+  const origOn = io.on.bind(io);
+  io.on = function(ev, cb) {
+    return origOn(ev, function(socket) {
+      const oldOn = socket.on.bind(socket);
+      socket.on = function(ev, cb) {
+        return oldOn(ev, function(...args) {
+          setTimeout(() => cb(...args), FAKE_LAG);
+        });
+      };
+      return cb(socket);
+    });
+  }
+}
 
 // Game Server
 const SpaaaceServerEngine = require(path.join(__dirname, 'src/server/SpaaaceServerEngine.js'));
