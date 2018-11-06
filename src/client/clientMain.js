992b8b36f23d00ec8360c2256016e9e0bd6e3035
namel <fourvelocity@gmail.com>
2018-11-06 11:16:23 +0200

    allow query params for server override

--- src/client/clientMain.js
+++ src/client/clientMain.js
@@ -1,19 +1,22 @@
+import querystring from 'query-string';
 import Trace from 'lance/lib/Trace';
 import SpaaaceClientEngine from './SpaaaceClientEngine';
 import SpaaaceGameEngine from '../common/SpaaaceGameEngine';
 import '../../assets/sass/main.scss';
+const qsOptions = querystring.parse(location.search);
 
 // sent to both game engine and client engine
-const options = {
+const defaults = {
     traceLevel: Trace.TRACE_NONE,
     delayInputCount: 8,
     scheduler: 'render-schedule',
     syncOptions: {
-        sync: 'extrapolate',
+        sync: qsOptions.sync || 'extrapolate',
         localObjBending: 0.2,
         remoteObjBending: 0.5
     }
 };
+let options = Object.assign(defaults, qsOptions);
 
 // create a client engine and a game engine
 const gameEngine = new SpaaaceGameEngine(options);
