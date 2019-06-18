1dcafd5c9d2411d036fec4d472fa49768fc59d60
namel <fourvelocity@gmail.com>
2019-06-18 10:43:52 +0300

    kill old process using pwdx

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
 cd /var/games/spaaace
-PORT=3001 npm start >spaaace.out 2>spaaace.err &
-      
+
+# arg 1: game name
+# arg 2: port name
+run-game () {
+    pgrep node | xargs pwdx | grep $1 |awk '{print $1}' | sed s/://g > /tmp/killpid
+    kill -9 `cat /tmp/killpid` || true
+    cd /var/games/$1
+    PORT=$2 npm start >$1.out 2>$1.err &
+}
+
+run-game spaaace 3001
--- scripts/npm-stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pkill -f spaaace || true
-
